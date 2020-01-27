d5ad23ae40854325931f6e59e86ed7f675261146
isaac2004 <isaac.r.levin@gmail.com>
2020-01-27 12:08:37 -0800

    clean up and check for lights

---
+++ src/PresenceLight.Core/Graph/ProtectedCallHelper.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresenceLight.Core.Graph
+{
+    /// <summary>
+    /// Helper class to call a protected API and process its result
+    /// </summary>
+    public class ProtectedApiCallHelper
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="httpClient">HttpClient used to call the protected API</param>
+        public ProtectedApiCallHelper(HttpClient httpClient)
+        {
+            HttpClient = httpClient;
+        }
+
+        protected HttpClient HttpClient { get; private set; }
+
+
+        /// <summary>
+        /// Calls the protected Web API and processes the result
+        /// </summary>
+        /// <param name="webApiUrl">Url of the Web API to call (supposed to return Json)</param>
+        /// <param name="accessToken">Access token used as a bearer security token to call the Web API</param>
+        /// <param name="processResult">Callback used to process the result of the call to the Web API</param>
+        public async Task CallWebApiAndProcessResultASync(string webApiUrl, string accessToken, Action<JObject> processResult)
+        {
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                var defaultRequetHeaders = HttpClient.DefaultRequestHeaders;
+                if (defaultRequetHeaders.Accept == null || !defaultRequetHeaders.Accept.Any(m => m.MediaType == "application/json"))
+                {
+                    HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                }
+                defaultRequetHeaders.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
+
+                HttpResponseMessage response = await HttpClient.GetAsync(webApiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    JObject result = JsonConvert.DeserializeObject(json) as JObject;
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    processResult(result);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Failed to call the Web Api: {response.StatusCode}");
+                    string content = await response.Content.ReadAsStringAsync();
+
+                    // Note that if you got reponse.Code == 403 and reponse.content.code == "Authorization_RequestDenied"
+                    // this is because the tenant admin as not granted consent for the application to call the Web API
+                    Console.WriteLine($"Content: {content}");
+                }
+                Console.ResetColor();
+            }
+        }
+    }
+}
