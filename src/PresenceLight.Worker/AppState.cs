f2ab9f1328112de69d9cf37c5708d68f9b1f6858
isaacrlevin <isaac.r.levin@gmail.com>
2020-03-24 15:30:50 -0700

    initial blazor work

---
+++ src/PresenceLight.Worker/AppState.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Graph;
+using Q42.HueApi;
+using Q42.HueApi.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresenceLight.Worker
+{
+    public class AppState
+    {
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public event Action OnChange;
+
+        public User User { get; set; }
+
+        public IEnumerable<Light> Lights { get; set; }
+
+        public string LightId { get; set; }
+
+        public string ProfileImage { get; set; }
+
+        public Presence Presence { get; set; }
+
+        public void SetUserInfo(User user, byte[] photo, Presence presence)
+        {
+            User = user;
+            Presence = presence;
+            var base64 = Convert.ToBase64String(photo);
+            var imgSrc = String.Format("data:image/gif;base64,{0}", base64);
+            ProfileImage = imgSrc;
+            NotifyStateChanged();
+        }
+
+        public void SetPresence(Presence presence)
+        {
+            Presence = presence;
+            NotifyStateChanged();
+        }
+
+        public void SetLights(IEnumerable<Light> lights)
+        {
+            Lights = lights;
+            NotifyStateChanged();
+        }
+
+        public void SetLight(string lightId)
+        {
+            LightId = lightId;
+            NotifyStateChanged();
+        }
+    }
+}
---
+++ src/PresenceLight.Worker/Startup.cs
@@ -0,0 +1,67 @@
+﻿using Blazored.Modal;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PresenceLight.Core;
+using PresenceLight.Core.Graph;
+
+namespace PresenceLight.Worker
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddOptions();
+            services.Configure<ConfigWrapper>(options => Configuration.Bind("AzureAd", options));
+            services.AddSingleton<IGraphService, GraphService>();
+            services.AddSingleton<IHueService, HueService>();
+            services.AddSingleton<AppState, AppState>();
+            services.AddBlazoredModal();
+            services.AddHttpContextAccessor();
+            services.AddHostedService<Worker>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
