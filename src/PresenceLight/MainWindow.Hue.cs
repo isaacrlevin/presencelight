a0a5f53b862560c5105df989ef4db070fa3205ee
isaacrlevin <isaac.r.levin@gmail.com>
2020-04-14 13:15:17 -0700

    Code Cleanup / Updating Dependencies

---
+++ src/PresenceLight/MainWindow.Hue.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using PresenceLight.Core;
+using System.Windows;
+using System.Windows.Media;
+using PresenceLight.Core.Helpers;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace PresenceLight
+{
+    public partial class MainWindow : Window
+    {
+        #region Hue Panel
+
+        private async void SaveHueSettings_Click(object sender, RoutedEventArgs e)
+        {
+            await SettingsService.SaveSettings(Config);
+            _hueService = new HueService(Config);
+            CheckHueSettings();
+            lblHueSaved.Visibility = Visibility.Visible;
+        }
+
+        private void ddlHueLights_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ddlHueLights.SelectedItem != null)
+            {
+                Config.SelectedHueLightId = ((Q42.HueApi.Light)ddlHueLights.SelectedItem).Id;
+                _options.SelectedHueLightId = Config.SelectedHueLightId;
+            }
+        }
+
+        private void HueIpAddress_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (((TextBox)e.OriginalSource).Text.Trim() != ((TextBox)e.Source).Text.Trim())
+            {
+                if (_options != null)
+                {
+                    _options.HueApiKey = String.Empty;
+                }
+                if (Config != null)
+                {
+                    Config.HueApiKey = String.Empty;
+                }
+            }
+            CheckHueSettings();
+        }
+        private async void CheckHueSettings()
+        {
+            if (Config != null)
+            {
+                if (!CheckAAD())
+                {
+                    configErrorPanel.Visibility = Visibility.Visible;
+                    dataPanel.Visibility = Visibility.Hidden;
+                    signInPanel.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    configErrorPanel.Visibility = Visibility.Hidden;
+                    signInPanel.Visibility = Visibility.Visible;
+
+                    if (_graphServiceClient == null)
+                    {
+                        _graphServiceClient = _graphservice.GetAuthenticatedGraphClient(typeof(WPFAuthorizationProvider));
+                    }
+                }
+
+                SolidColorBrush fontBrush = new SolidColorBrush();
+
+
+                if (!CheckHueIp())
+                {
+                    lblHueMessage.Text = "Valid IP Address Required";
+                    fontBrush.Color = MapColor("#ff3300");
+                    btnRegister.IsEnabled = false;
+                    ddlHueLights.Visibility = Visibility.Collapsed;
+                    lblHueMessage.Foreground = fontBrush;
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(Config.HueIpAddress))
+                    {
+                        Config.HueIpAddress = hueIpAddress.Text;
+                    }
+
+                    if (string.IsNullOrEmpty(_options.HueIpAddress))
+                    {
+                        _options.HueIpAddress = hueIpAddress.Text;
+                    }
+                    btnRegister.IsEnabled = true;
+                    if (string.IsNullOrEmpty(Config.HueApiKey))
+                    {
+                        lblHueMessage.Text = "Missing App Registration, please button on bridge than click 'Register Bridge'";
+                        fontBrush.Color = MapColor("#ff3300");
+                        ddlHueLights.Visibility = Visibility.Collapsed;
+                        lblHueMessage.Foreground = fontBrush;
+                    }
+                    else
+                    {
+                        ddlHueLights.ItemsSource = await _hueService.CheckLights();
+
+                        foreach (var item in ddlHueLights.Items)
+                        {
+                            var light = (Q42.HueApi.Light)item;
+                            if (light.Id == Config.SelectedHueLightId)
+                            {
+                                ddlHueLights.SelectedItem = item;
+                            }
+                        }
+                        ddlHueLights.Visibility = Visibility.Visible;
+                        lblHueMessage.Text = "App Registered with Bridge";
+                        fontBrush.Color = MapColor("#009933");
+                        lblHueMessage.Foreground = fontBrush;
+                    }
+                }
+            }
+        }
+
+        private bool CheckHueIp()
+        {
+            string r2 = @"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b";
+
+            Regex r = new Regex(r2);
+
+            if (string.IsNullOrEmpty(hueIpAddress.Text.Trim()) || !r.IsMatch(hueIpAddress.Text.Trim()) || hueIpAddress.Text.Trim().EndsWith("."))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool CheckAAD()
+        {
+            Regex r = new Regex(@"^(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}$");
+            if (string.IsNullOrEmpty(Config.ClientId) || string.IsNullOrEmpty(Config.TenantId) || string.IsNullOrEmpty(Config.RedirectUri) || !r.IsMatch(Config.ClientId) || !r.IsMatch(Config.TenantId))
+            {
+                return false;
+            }
+
+            _options.ClientId = Config.ClientId;
+            _options.TenantId = Config.TenantId;
+            _options.RedirectUri = Config.RedirectUri;
+
+            return true;
+        }
+
+        private async void FindBridge_Click(object sender, RoutedEventArgs e)
+        {
+            hueIpAddress.Text = await _hueService.FindBridge();
+        }
+
+        private void cbIsPhillipsEnabledChanged(object sender, RoutedEventArgs e)
+        {
+            if (Config.IsPhillipsEnabled)
+            {
+                pnlPhillips.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                pnlPhillips.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private async void RegisterBridge_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxHelper.PrepToCenterMessageBoxOnForm(this);
+            MessageBox.Show("Please press the sync button on your Phillips Hue Bridge");
+
+            SolidColorBrush fontBrush = new SolidColorBrush();
+
+            try
+            {
+                imgLoading.Visibility = Visibility.Visible;
+                lblHueMessage.Visibility = Visibility.Collapsed;
+                if (string.IsNullOrEmpty(_options.HueIpAddress))
+                {
+                    _options.HueIpAddress = Config.HueIpAddress;
+                }
+                Config.HueApiKey = await _hueService.RegisterBridge();
+                ddlHueLights.ItemsSource = await _hueService.CheckLights();
+                _options.HueApiKey = Config.HueApiKey;
+                ddlHueLights.Visibility = Visibility.Visible;
+                imgLoading.Visibility = Visibility.Collapsed;
+                lblHueMessage.Visibility = Visibility.Visible;
+            }
+            catch (Exception ex)
+            {
+                lblHueMessage.Text = "Error Occured registering bridge, please try again";
+                fontBrush.Color = MapColor("#ff3300");
+                lblHueMessage.Foreground = fontBrush;
+            }
+
+            if (!string.IsNullOrEmpty(Config.HueApiKey))
+            {
+                lblHueMessage.Text = "App Registered with Bridge";
+                fontBrush.Color = MapColor("#009933");
+                lblHueMessage.Foreground = fontBrush;
+            }
+
+            CheckHueSettings();
+        }
+
+        #endregion
+    }
+}
---
+++ src/PresenceLight/MainWindow.Lifx.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using LifxCloud.NET.Models;
+
+namespace PresenceLight
+{
+    public partial class MainWindow : Window
+    {
+        #region Lifx Panel
+
+        private async void SaveLifxSettings_Click(object sender, RoutedEventArgs e)
+        {
+            await SettingsService.SaveSettings(Config);
+            lblLifxSaved.Visibility = Visibility.Visible;
+        }
+
+        private async void CheckLifxSettings()
+        {
+            SolidColorBrush fontBrush = new SolidColorBrush();
+            try
+            {
+                if (Config.IsLifxEnabled && !string.IsNullOrEmpty(Config.LifxApiKey) && !string.IsNullOrEmpty(Config.SelectedLifxItemId))
+                {
+                    ddlLifxLights.ItemsSource = await _lifxService.GetAllLightsAsync();
+
+                    foreach (var item in ddlLifxLights.Items)
+                    {
+                        var light = (Light)item;
+                        if ($"id:{light.Id}" == Config.SelectedLifxItemId)
+                        {
+                            ddlLifxLights.SelectedItem = item;
+                        }
+                    }
+
+                    if (ddlLifxLights.SelectedItem == null)
+                    {
+                        ddlLifxLights.ItemsSource = await _lifxService.GetAllGroupsAsync();
+
+                        foreach (var item in ddlLifxLights.Items)
+                        {
+                            var group = (LifxCloud.NET.Models.Group)item;
+                            if ($"group_id:{group.Id}" == Config.SelectedLifxItemId)
+                            {
+                                ddlLifxLights.SelectedItem = item;
+                            }
+                        }
+                    }
+
+                    if (ddlLifxLights.SelectedItem != null)
+                    {
+                        
+                        ddlLifxLights.Visibility = Visibility.Visible;
+                        lblLifxMessage.Text = "Connected to Lifx Cloud";
+                        fontBrush.Color = MapColor("#009933");
+                        lblLifxMessage.Foreground = fontBrush;
+                    }
+                }
+            }
+            catch
+            {
+                lblLifxMessage.Text = "Error Occured Connecting to Lifx, please try again";
+                fontBrush.Color = MapColor("#ff3300");
+                lblLifxMessage.Foreground = fontBrush;
+            }
+        }
+
+        private void ddlLifxLights_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ddlLifxLights.SelectedItem != null)
+            {
+                // Get whether item is group or light
+                if (ddlLifxLights.SelectedItem.GetType() == typeof(LifxCloud.NET.Models.Group))
+                {
+                    Config.SelectedLifxItemId = $"group_id:{((LifxCloud.NET.Models.Group)ddlLifxLights.SelectedItem).Id}";
+                }
+
+                if (ddlLifxLights.SelectedItem.GetType() == typeof(LifxCloud.NET.Models.Light))
+                {
+                    Config.SelectedLifxItemId = $"id:{((LifxCloud.NET.Models.Light)ddlLifxLights.SelectedItem).Id}";
+
+                }
+                _options.SelectedLifxItemId = Config.SelectedLifxItemId;
+
+            }
+        }
+
+        private async void CheckLifx_Click(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush fontBrush = new SolidColorBrush();
+
+            if (!string.IsNullOrEmpty(lifxApiKey.Text))
+            {
+                try
+                {
+                    _options.LifxApiKey = lifxApiKey.Text;
+                    Config.LifxApiKey = lifxApiKey.Text;
+
+                    if (((System.Windows.Controls.Button)sender).Name == "btnGetLifxGroups")
+                    {
+                        ddlLifxLights.ItemsSource = await _lifxService.GetAllGroupsAsync();
+                    }
+                    else
+                    {
+                        ddlLifxLights.ItemsSource = await _lifxService.GetAllLightsAsync();
+                    }
+
+                    ddlLifxLights.Visibility = Visibility.Visible;
+                    lblLifxMessage.Text = "Connected to Lifx Cloud";
+                    fontBrush.Color = MapColor("#009933");
+                    lblLifxMessage.Foreground = fontBrush;
+                }
+                catch
+                {
+                    ddlLifxLights.Visibility = Visibility.Collapsed;
+                    lblLifxMessage.Text = "Error Occured Connecting to Lifx, please try again";
+                    fontBrush.Color = MapColor("#ff3300");
+                    lblLifxMessage.Foreground = fontBrush;
+                }
+            }
+            else
+            {
+
+                Run run1 = new Run("Valid Lifx Key Required ");
+                Run run2 = new Run(" https://cloud.lifx.com/settings");
+
+                Hyperlink hyperlink = new Hyperlink(run2)
+                {
+                    NavigateUri = new Uri("https://cloud.lifx.com/settings")
+                };
+                hyperlink.RequestNavigate += new System.Windows.Navigation.RequestNavigateEventHandler(Hyperlink_RequestNavigate); //to be implemented
+                lblLifxMessage.Inlines.Clear();
+                lblLifxMessage.Inlines.Add(run1);
+                lblLifxMessage.Inlines.Add(hyperlink);
+
+
+                fontBrush.Color = MapColor("#ff3300");
+                lblLifxMessage.Foreground = fontBrush;
+
+            }
+        }
+
+        private void cbIsLifxEnabledChanged(object sender, RoutedEventArgs e)
+        {
+            if (Config.IsLifxEnabled)
+            {
+                pnlLifx.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                pnlLifx.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        #endregion
+
+
+    }
+}
