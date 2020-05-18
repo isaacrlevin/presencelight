e9979b97f53fdd9c53d989a83ba0a5ec0b16e441
Isaac Levin <8878502+isaacrlevin@users.noreply.github.com>
2020-05-17 20:13:22 -0700

    Update ChocolateyInstall.ps1

--- .chocolatey/tools/ChocolateyInstall.ps1
+++ .chocolatey/tools/ChocolateyInstall.ps1
@@ -2,11 +2,7 @@ $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installPath = Join-Path $toolsDir 'PresenceLight'
 $exePath = Join-Path $toolsDir 'PresenceLight\PresenceLight.exe'
-$zipPath = Join-Path $toolsDir 'Release.zip'
 
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$zipPath" `
-                             -UnzipLocation "$installPath"
 
 Write-Output "Adding shortcut to Start Menu"
 Install-ChocolateyShortcut -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PresenceLight.lnk" -TargetPath $exePath
