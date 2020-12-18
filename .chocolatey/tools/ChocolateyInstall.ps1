cc29f8645045eaece2fd430ed9de007d77d4a99b
Isaac Levin <isaac.r.levin@gmail.com>
2020-12-18 11:48:16 -0800

    Updates to Azure Pipelines

--- .chocolatey/tools/ChocolateyInstall.ps1
+++ .chocolatey/tools/ChocolateyInstall.ps1
@@ -1,8 +1,5 @@
-$ErrorActionPreference = 'Stop';
+$ErrorActionPreference  = 'Stop';
 
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName       = "$toolsDir\PackagePath"
-$version        = "ReplaceVersion"
 
 $WindowsVersion=[Environment]::OSVersion.Version
 if ($WindowsVersion.Major -ne "10") {
@@ -14,15 +11,43 @@ if ($IsCorrectBuild -lt "17134") {
   throw "This package requires at least Windows 10 version build 17134.x."
 }
 
-if ((Get-AppxPackage -name 37828IsaacLevin.197278F15330A).Version -Match $version) {
-  if($env:ChocolateyForce) {
-    # you can't install the same version of an appx package, you need to remove it first
-    Write-Host Removing already installed version first.
-    Get-AppxPackage -Name 37828IsaacLevin.197278F15330A | Remove-AppxPackage
-  } else {
-    Write-Host The $version version of PresenceLight is already installed. If you want to reinstall use --force
-    return
-  }
+$version        = "{ReplaceVersion}"
+
+$packageName    = "presencelight"
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url_x86        = "https://github.com/isaacrlevin/PresenceLight/releases/download/v${version}/PresenceLight.${version}-x32.zip"
+$url_x64        = "https://github.com/isaacrlevin/PresenceLight/releases/download/v${version}/PresenceLight.${version}-x64.zip"
+$checksum_x86   = "{ReplaceCheckSumx86}"
+$checksum_x64   = "{ReplaceCheckSumx64}"
+
+# By default, we want 32-bit installer
+$url = $url_x86
+$checksum = $checksum_x86
+
+# If specifically asked for 64-bit, we use that
+$pp = Get-PackageParameters
+if ($pp['x64'] -eq 'true')
+{
+  $url = $url_x64
+  $checksum = $checksum_x64
 }
 
-Add-AppxPackage -Path $fileName
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'SHA512'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$installPath    = Join-Path $toolsDir 'PresenceLight'
+$exePath = Join-Path installPath 'PresenceLight.exe'
+
+
+Write-Output "Adding shortcut to Start Menu"
+Install-ChocolateyShortcut -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PresenceLight.lnk" -TargetPath $exePath
+
+Write-Output "Adding shortcut to Startup"
+Install-ChocolateyShortcut -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\PresenceLight.lnk" -TargetPath $exePath
