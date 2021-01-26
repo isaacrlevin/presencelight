73f96a663f70c9744e225c8a1bc00726c1fc6ad1
Isaac Levin <isaac.r.levin@gmail.com>
2021-01-26 12:19:18 -0800

    More cleanup

--- src/DesktopClient/PresenceLight/Telemetry/AppVersionTelemetryInitializer.cs
+++ src/DesktopClient/PresenceLight/Telemetry/AppVersionTelemetryInitializer.cs
@@ -7,18 +7,18 @@ namespace PresenceLight.Telemetry
 {
     internal class AppVersionTelemetryInitializer : ITelemetryInitializer
     {
-        private readonly string _wpfVersion;        
+        private readonly string _runtimeVersion;        
         private readonly string _appVersion;
 
         public AppVersionTelemetryInitializer()
         {
-            _wpfVersion = ThisAppInfo.GetDotNetRuntimeInfo();
+            _runtimeVersion = ThisAppInfo.GetDotNetRuntimeInfo();
             _appVersion = ThisAppInfo.GetPackageVersion();
         }
 
         public void Initialize(ITelemetry telemetry)
         {            
-            telemetry.Context.GlobalProperties[".NET Runtime Version"] = _wpfVersion;            
+            telemetry.Context.GlobalProperties[".NET Runtime Version"] = _runtimeVersion;            
             telemetry.Context.Component.Version = _appVersion;
         }
     }
