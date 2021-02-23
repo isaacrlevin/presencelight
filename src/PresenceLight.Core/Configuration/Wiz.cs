df8d0ba116c806d263d30072471e0deb36f696b0
Isaac Levin <8878502+isaacrlevin@users.noreply.github.com>
2021-02-23 09:58:26 -0800

    Wiz Light Support (#283)

---
+++ src/PresenceLight.Core/Configuration/Wiz.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PresenceLight.Core
+{
+    public class Wiz : BaseLight
+    {
+    }
+}
---
+++ src/PresenceLight.Core/Lights/WizServices/GetLights/GetLightsCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+using MediatR;
+
+using OpenWiz;
+
+namespace PresenceLight.Core.WizServices
+{
+    public class GetLightsCommand : IRequest<IEnumerable<WizLight>>
+    {
+    }
+}
