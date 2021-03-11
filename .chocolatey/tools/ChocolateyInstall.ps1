498656f08f4d3857d7606aec049c5229e2713626
Logan Long <loganjohnlong@users.noreply.github.com>
2021-03-11 11:45:13 -0600

    Update ChocolateyInstall.ps1
    Add support for autodetecting architecture during Chocolatey install

--- .chocolatey/tools/ChocolateyInstall.ps1
+++ .chocolatey/tools/ChocolateyInstall.ps1
@@ -13,21 +13,25 @@ if ($IsCorrectBuild -lt "17134") {
 
 $packageName    = "presencelight"
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url_x86        = "{x86Link}"
-$url_x64        = "{x64Link}"
-$checksum_x86   = "{ReplaceCheckSumx86}"
-$checksum_x64   = "{ReplaceCheckSumx64}"
+$url_x86        = "https://github.com/isaacrlevin/presencelight/releases/download/Desktop-v4.0.1/PresenceLight.4.0.1-x86.zip"
+$url_x64        = "https://github.com/isaacrlevin/presencelight/releases/download/Desktop-v4.0.1/PresenceLight.4.0.1-x64.zip"
+$checksum_x86   = "579CEBA3771C0287C268D8D80E1C6FB2BA5D91CA6C87C1C0666540CA2D509A91"
+$checksum_x64   = "1055A76F5E5500D0BC4D8617DF6928487558F55A6DF467D7EC5EFA47492C08BB"
 
-# By default, we want 32-bit installer
-$url = $url_x86
-$checksum = $checksum_x86
-
-# If specifically asked for 64-bit, we use that
+# Detect Architecture automatically
+# Respect user choice first 
 $pp = Get-PackageParameters
-if ($pp['x64'] -eq 'true')
+$Is64Bit = [Environment]::Is64BitOperatingSystem
+if ($pp['x86'] -eq 'true' -Or $Is64Bit -eq 'false')
 {
+  $url = $url_x86
+  $checksum = $checksum_x86
+}elseif ($pp['x64'] -eq 'true' -Or $Is64Bit -eq 'true') {
   $url = $url_x64
   $checksum = $checksum_x64
+}else { # Default to 32 bit system
+  $url = $url_x86
+  $checksum = $checksum_x86
 }
 
 $packageArgs = @{
@@ -48,3 +52,4 @@ Install-ChocolateyShortcut -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\S
 
 Write-Output "Adding shortcut to Startup"
 Install-ChocolateyShortcut -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\PresenceLight.lnk" -TargetPath $exePath
+
