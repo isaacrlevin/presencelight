5d24632ba8d6b7f7829ce6346577381759c93b9f
Stephen Valdinger <stephen@chocolatey.io>
2021-03-23 18:28:15 -0700

    Fix chocolatey package to respect path of launched exe, and tidy uninstall script

--- .chocolatey/tools/ChocolateyUninstall.ps1
+++ .chocolatey/tools/ChocolateyUninstall.ps1
@@ -1,1 +1,9 @@
-﻿Uninstall-ChocolateyPackage  -PackageName "presencelight"
+$light = Get-process presencelight*
+
+if($light){
+	$light | Stop-Process -Force
+}
+
+Remove-Item $Env:AppData\PresenceLight -Recurse -Force
+Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PresenceLight.lnk"
+Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\PresenceLight.lnk"
