f558460085824acabb10dacd7894f0d6d13cc89e
isaacrlevin <isaac.r.levin@gmail.com>
2021-08-27 11:37:39 -0700

    initial work for MAUI app

---
+++ src/PresenceLight.Razor/Services/AppVersionTelemetryInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace PresenceLight.Razor.Services
+{
+    internal class AppVersionTelemetryInitializer : ITelemetryInitializer
+    {
+        private readonly string _appVersion;
+
+        public AppVersionTelemetryInitializer()
+        {
+            _appVersion = ThisAppInfo.GetApplicationVersion();
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.GlobalProperties["App Version"] = "Worker";
+            telemetry.Context.Component.Version = _appVersion;
+        }
+    }
+}
---
+++ src/PresenceLight.Razor/Services/ThisAppInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+
+namespace PresenceLight
+{
+    internal class ThisAppInfo
+    {
+        internal static string GetInstallLocation()
+        {
+            return System.AppContext.BaseDirectory;
+        }
+
+        internal static string GetInstallationDate()
+        {
+            var date = System.IO.File.GetLastWriteTime(System.AppContext.BaseDirectory);
+            return $"{date.ToShortDateString()} {date.ToShortTimeString()}";
+        }
+
+        public static string GetApplicationVersion()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fileVersion = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return fileVersion.FileVersion;
+        }
+
+        internal static string GetDotNetRuntimeInfo()
+        {
+            return typeof(object).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        }
+    }
+}
