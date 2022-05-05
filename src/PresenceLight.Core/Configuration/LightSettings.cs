e0b74e0151f1f4994add8116d0f5e69fb18c73a2
Isaac Levin <8878502+isaacrlevin@users.noreply.github.com>
2022-05-05 15:28:24 -0700

    Fix AmPm Bug

--- src/PresenceLight.Core/Configuration/LightSettings.cs
+++ src/PresenceLight.Core/Configuration/LightSettings.cs
@@ -15,7 +15,7 @@ namespace PresenceLight.Core
 
         public bool UseWorkingHours { get; set; }
 
-        public bool UseAmPM { get; set; }
+        public bool UseAmPm { get; set; }
 
         public string WorkingHoursStartTime { get; set; }
 
