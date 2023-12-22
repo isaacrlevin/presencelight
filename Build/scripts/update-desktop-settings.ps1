d0b9f9beb44a35fc3687af6f658cf11a80b3cdbd
Isaac Levin <isaac.r.levin@gmail.com>
2023-12-22 11:08:36 -0800

    fix

--- Build/scripts/update-desktop-settings.ps1
+++ Build/scripts/update-desktop-settings.ps1
@@ -42,29 +42,29 @@ Param
 
 switch ($Release) {
     "Release" {
-        # Update appxmanifest. This must be done before build.
+        Write-Host "Updating AppxManifest for Release"
         [xml]$manifest = get-content ".\src\DesktopClient\PresenceLight.Package\Package.appxmanifest"
         $manifest.Package.Identity.Version = "${Version}.0"
         $manifest.save(".\src\DesktopClient\PresenceLight.Package\Package.appxmanifest")
 
-        # Update AppSettings.json. This must be done before build.
+        Write-Host "Updating AppSettings for Release"
         $appsettings = get-content ".\src\DesktopClient\PresenceLight\appsettings.json" -raw | ConvertFrom-Json
         $appsettings.isAppPackaged = "true"
         $appsettings | ConvertTo-Json -depth 32 | set-content '.\src\DesktopClient\PresenceLight\appsettings.json'
     }
     "Nightly" {
-        # Update appxmanifest. This must be done before build.
+        Write-Host "Updating AppxManifest for Nightly"
         [xml]$manifest = Get-Content ".\src\DesktopClient\PresenceLight.Package\Package-Nightly.appxmanifest"
         $manifest.Package.Identity.Version = "${Version}.0"
         $manifest.Save(".\src\DesktopClient\PresenceLight.Package\Package-Nightly.appxmanifest")
 
-        # Update AppSettings.json. This must be done before build.
+        Write-Host "Updating AppSettings for Nightly"
         $appsettings = get-content ".\src\DesktopClient\PresenceLight\appsettings.json" -raw | ConvertFrom-Json
         $appsettings.isAppPackaged = "true"
         $appsettings | ConvertTo-Json -depth 32 | set-content '.\src\DesktopClient\PresenceLight\appsettings.json'
     }
     "Standalone" {
-        # Update AppSettings.json. This must be done before build.
+        Write-Host "Updating AppSettings for Standalone"
         $appsettings = get-content ".\src\DesktopClient\PresenceLight\appsettings.json" -raw | ConvertFrom-Json
         $appsettings.isAppPackaged = "false"
         $appsettings | ConvertTo-Json -depth 32 | set-content '.\src\DesktopClient\PresenceLight\appsettings.json'
@@ -72,7 +72,7 @@ switch ($Release) {
 }
 
 
-# Update AppSettings.json. This must be done before build.
+Write-Host "Updating AppSettings for All Channels"
 $appsettings = get-content ".\src\DesktopClient\PresenceLight\appsettings.json" -raw | ConvertFrom-Json
 $appsettings.aadSettings.clientId = "${ApplicationId}"
 $appsettings.appVersion = "${Version}"
